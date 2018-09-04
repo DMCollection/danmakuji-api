3730dc02624b30656f1926da1fbefd0e261d2e2d
echisan <xuan1916152345@gmail.com>
2018-09-04 15:11:58 +0800

    修复了处理举报内容是没有给当事人发通知的问题

--- src/main/java/cc/dmji/api/enums/v2/ReplyType.java
+++ src/main/java/cc/dmji/api/enums/v2/ReplyType.java
@@ -5,27 +5,30 @@ public enum ReplyType {
     /**
      * 番剧某一集下的评论
      */
-    BANGUMI_EPISODE(1),
+    BANGUMI_EPISODE(1,"/#/video/"),
 
     /**
      * 公告下的评论
      */
-    NOTICE(2),
+    NOTICE(2,"/#/announce/"),
 
     /**
      * 番剧下的评论
      */
-    BANGUMI(3),
+    BANGUMI(3,"/#/bangumi/"),
 
     /**
      * 用户个人资料页面下的评论
      */
-    USER(4);
+    USER(4,"/#/user/");
 
     Integer code;
 
-    ReplyType(Integer code) {
+    String localUrl;
+
+    ReplyType(Integer code, String localUrl) {
         this.code = code;
+        this.localUrl = localUrl;
     }
 
     public Integer getCode() {
